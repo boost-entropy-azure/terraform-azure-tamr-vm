839f6719468ca717f3ae23c13062579cd9fabc2e
krisVIBES <brethower.k@gmail.com>
2020-07-20 16:32:44 -0400

    add redhat custom image example

---
+++ examples/redhat-custom-image/main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_resource_group" "vm-rg" {
+  name     = "tamrVmResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vm-vnet" {
+  name = "tamrVmVirtualNetwork"
+
+  location            = azurerm_resource_group.vm-rg.location
+  resource_group_name = azurerm_resource_group.vm-rg.name
+
+  address_space = ["1.2.3.0/25"]
+}
+
+resource "azurerm_subnet" "vm-subnet" {
+  name = "tamrVmSubnet"
+
+  resource_group_name = azurerm_resource_group.vm-rg.name
+
+  virtual_network_name = azurerm_virtual_network.vm-vnet.name
+  address_prefixes     = ["1.2.3.0/28"]
+}
+
+resource "azurerm_application_security_group" "sg" {
+  name                = "vmApplicationSG"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.vm-rg.name
+}
+
+module "vm" {
+  source = "../../"
+
+  vm_name             = "redhat-example-vm"
+  resource_group_name = azurerm_resource_group.vm-rg.name
+  location            = azurerm_resource_group.vm-rg.location
+  subnet_id           = azurerm_subnet.vm-subnet.id
+  vm_size             = "Standard_D2s_v3"
+  managed_disk_type   = "Premium_LRS"
+  disk_size_gb        = 100
+
+  image_publisher     = "RedHat"
+  image_offer         = "RHEL"
+  image_sku           = "7.6"
+  image_version       = "latest"
+
+  ingress_cidr_blocks            = ["2.3.4.5/32"]
+  application_security_group_ids = [azurerm_application_security_group.sg.id]
+
+  admin_username = "fakeUsername"
+  ssh_key_data   = file("~/.ssh/id_rsa.pub")
+}
---
+++ examples/redhat-custom-image/outputs.tf
@@ -0,0 +1,18 @@
+output "vm-module" {
+  value       = module.vm
+  description = "All resources created by the VM module"
+}
+output "vm-vnet-id" {
+  value       = azurerm_virtual_network.vm-vnet.id
+  description = "The virtual network created for the VM"
+}
+
+output "vm-resource-group-id" {
+  value       = azurerm_resource_group.vm-rg.id
+  description = "The resource group created for the VM"
+}
+
+output "vm-subnet-id" {
+  value       = azurerm_subnet.vm-subnet.id
+  description = "The subnet created for the VM"
+}
