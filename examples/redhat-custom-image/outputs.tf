27b166ee6486d4c5ae7cf038273b6298650c283c
2-X <brethower.k@gmail.com>
2020-08-04 18:58:53 -0400

    update version and changelog, fix example outputs

--- examples/redhat-custom-image/outputs.tf
+++ examples/redhat-custom-image/outputs.tf
@@ -8,7 +8,7 @@ output "vm-vnet-id" {
 }
 
 output "vm-resource-group-id" {
-  value       = data.azurerm_resource_group.vm-rg.id
+  value       = azurerm_resource_group.vm-rg.id
   description = "The resource group created for the VM"
 }
 
